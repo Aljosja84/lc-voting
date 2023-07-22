24a3abfb791c348572eecdf1cae4f3ed83a828ca
Aljosja84 <gabriel.gressie@gmail.com>
2023-07-22 22:47:50 +0200

    couple of feature tests, all passing

--- tests/Feature/ShowIdeas.php
+++ tests/Feature/ShowIdeas.php
@@ -2,21 +2,46 @@
 
 namespace Tests\Feature;
 
+use App\Models\Idea;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ShowIdeas extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    public function list_of_ideas_shows_on_main_page()
+    {
+        $firstIdea = Idea::factory()->create([
+            'title' => 'first idea title',
+            'description' => 'first idea description'
+        ]);
+
+        $secondIdea = Idea::factory()->create([
+            'title' => 'second idea title',
+            'description' => 'second idea description'
+        ]);
+
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee($firstIdea->title);
+        $response->assertSee($secondIdea->title);
+    }
+
+    /** @test */
+    public function single_idea_shows_on_show_page()
+    {
+        $idea = Idea::factory()->create([
+            'title' => 'first idea title',
+            'description' => 'first idea description'
+        ]);
+
+        $response = $this->get(route('idea.show', $idea));
+
+        $response->assertSuccessful();
+        $response->assertSee($idea->title);
     }
 }
