59ac420d3b08ace919b6ef0c5d071ab06addb3d5
Aljosja84 <gabriel.gressie@gmail.com>
2023-08-06 23:40:49 +0200

    status count in navbar now works

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -33,6 +33,6 @@
         @endforeach
     </div> <!-- end ideas-container -->
     <div class="my-8">
-        {{ $ideas->links() }}
+        {{ $ideas->appends(request()->query())->links() }}
     </div>
 </div>
