b64193271c690cdf333d55df3b1aecb46c77d1d3
Aljosja84 <gabriel.gressie@gmail.com>
2023-08-08 23:13:13 +0200

    some tests for the navbar filters

---
+++ tests/Feature/StatusFiltersTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\Idea;
+use App\Models\Status;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class StatusFiltersTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function index_page_contains_status_filters_lw_component()
+    {
+        $user = User::factory()->create();
+
+        $category = Category::factory()->create(['name' => 'Category 1']);
+
+        $status = Status::factory()->create(['name' => 'Open']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $category->id,
+            'status_id' => $status->id,
+            'title' => 'My idea',
+            'description' => 'description'
+        ]);
+
+        $this->get(route('idea.index'))
+            ->assertSeeLivewire('status-filters');
+    }
+
+    /** @test */
+    public function show_page_contains_status_filters_lw_component()
+    {
+        $user = User::factory()->create();
+
+        $category = Category::factory()->create(['name' => 'Category 1']);
+
+        $status = Status::factory()->create(['name' => 'Open']);
+
+        $idea = Idea::factory()->create([
+            'user_id' => $user->id,
+            'category_id' => $category->id,
+            'status_id' => $status->id,
+            'title' => 'My idea',
+            'description' => 'description'
+        ]);
+
+        $this->get(route('idea.show', $idea))
+            ->assertSeeLivewire('status-filters');
+    }
+}
