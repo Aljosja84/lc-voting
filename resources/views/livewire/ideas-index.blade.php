233888492f6cf943b83af7b8079f39ab70b49119
Aljosja84 <gabriel.gressie@gmail.com>
2023-08-15 20:18:09 +0200

    manual searching is working correctly

--- resources/views/livewire/ideas-index.blade.php
+++ resources/views/livewire/ideas-index.blade.php
@@ -16,7 +16,7 @@
             </select>
         </div>
         <div class="w-2/3 relative">
-            <input type="search" placeholder="Find an idea" class="w-full rounded-xl placeholder-gray-900 border-none px-4 py-2 pl-8">
+            <input wire:model="search" type="search" placeholder="Find an idea" class="w-full rounded-xl placeholder-gray-900 border-none px-4 py-2 pl-8">
             <div class="absolute top-0 flex items-center h-full ml-2">
                 <svg class="w-4 text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
@@ -26,10 +26,12 @@
     </div>
     <!-- idea cards -->
     <div class="ideas-container space-y-6 my-6">
-        @foreach($ideas as $idea)
+        @forelse($ideas as $idea)
             <livewire:idea-index :key="$idea->id" :idea="$idea" :votesCount="$idea->votes_count"/>
+        @empty
+            <div class="mx-auto w-70 h-16 bg-white text-center pt-6">No ideas found :(</div>
             <!-- end idea container -->
-        @endforeach
+        @endforelse
     </div> <!-- end ideas-container -->
     <div class="my-8">
         {{ $ideas->appends(request()->query())->links() }}
