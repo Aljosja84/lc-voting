95f92e691a926306490123eca8b661009c4d8fe5
Aljosja84 <gabriel.gressie@gmail.com>
2023-09-22 19:47:25 +0200

    some changes to spam before I alter the db structure

--- app/Http/Livewire/MarkIdeaAsSpam.php
+++ app/Http/Livewire/MarkIdeaAsSpam.php
@@ -2,10 +2,29 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Idea;
+use Illuminate\Http\Response;
 use Livewire\Component;
 
 class MarkIdeaAsSpam extends Component
 {
+    public $idea;
+
+    public function mount(Idea $idea)
+    {
+        $this->idea = $idea;
+    }
+
+    public function markAsSpam()
+    {
+        // quick auth
+        if(!auth()->check()) {
+            abort(Response::HTTP_FORBIDDEN);
+        }
+
+        $this->idea->spam_reports++;
+    }
+
     public function render()
     {
         return view('livewire.mark-idea-as-spam');
