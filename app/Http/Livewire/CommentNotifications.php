76bbf77316ab0c3c030cb810bbe4717bf11e582b
Aljosja84 <gabriel.gressie@gmail.com>
2023-11-20 23:42:44 +0100

    notification badge scaffolding

--- app/Http/Livewire/CommentNotifications.php
+++ app/Http/Livewire/CommentNotifications.php
@@ -7,16 +7,28 @@ use Livewire\Component;
 class CommentNotifications extends Component
 {
     public $notifications;
+    public $notificationCount;
+    public $notification_threshold = 3;
 
     protected $listeners = ['getNotifications'];
 
     public function mount() {
         $this->notifications = collect([]);
+        $this->getNotificationCount();
+    }
+
+    public function getNotificationCount()
+    {
+        $this->notificationCount = auth()->user()->unreadNotifications()->count();
+
+        if($this->notificationCount > $this->notification_threshold) {
+            $this->notificationCount = $this->notification_threshold.'+';
+        }
     }
 
     public function getNotifications()
     {
-        $this->notifications = auth()->user()->unreadNotifications;
+        $this->notifications = auth()->user()->unreadNotifications()->latest();
     }
 
     public function render()
